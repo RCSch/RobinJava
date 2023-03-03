a53a4953a45ab696e02e5bde1bc671877eaabb4c
robinschneidermann@gmail.com <robinschneidermann@gmail.com>
2023-03-03 01:11:59 +0100

    Alle elementerne er på interface nu.

--- app/src/main/java/com/example/robinjava/PersonService.java
+++ app/src/main/java/com/example/robinjava/PersonService.java
@@ -4,18 +4,30 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface PersonService
 {
+        //Create
+        @POST("Person/")
+        Call<Void> addElev(@Body Person e);
+
+        //Read
         @GET("Person/")
         Call<List<Person>> getAllPersoner();
 
         @GET("Person/{id}")
         Call<Person> getPersonById(@Path("id") int id);
 
-        @POST("Person/")
-        Call<Void> addElev(@Body Person e);
+        //Update
+        @PUT("Person")
+        Call<Void> updatePerson(@Body Person p);
+
+        //Delete
+        @DELETE("Person/{id}")
+        Call<Void> deletePersonById(@Path("id") int id);
 }
